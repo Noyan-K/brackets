565b93c870f60df0bba48d64098666e056388460
Noyan Kuzherbayev <noyan.ku@gmail.com>
2020-10-26 00:12:04 +0500

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let matrix = bracketsConfig.map(item => item.join(''));
+  for (let i = 0; i < matrix.length; i++){
+    if (str.includes(matrix[i])) {
+      str = str.replace(matrix[i],'');
+      i = -1;
+    }
+  }
+  return !str.length;
 }
